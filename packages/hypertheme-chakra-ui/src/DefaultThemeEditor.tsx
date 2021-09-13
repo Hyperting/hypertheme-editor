668b4f390676e380e7cbd67dab92afb496f86669
marco <marcon@hyperting.com>
2021-09-13 15:09:48 +0200

    feat: DefaultThemeEditor now accepts ThemeEditorDrawerButton props

--- packages/hypertheme-chakra-ui/src/DefaultThemeEditor.tsx
+++ packages/hypertheme-chakra-ui/src/DefaultThemeEditor.tsx
@@ -1,16 +1,21 @@
 import React, { FC } from 'react'
-import { ThemeEditor, ThemeEditorButton, ThemeEditorDrawer, ThemeEditorProps } from '@hypertheme-editor/chakra-ui-core'
+import {
+  ThemeEditor,
+  ThemeEditorButton,
+  ThemeEditorButtonProps,
+  ThemeEditorDrawer,
+} from '@hypertheme-editor/chakra-ui-core'
 import { ThemeEditorColors } from '@hypertheme-editor/chakra-ui-colors'
 import { ThemeEditorFontSizes } from '@hypertheme-editor/chakra-ui-font-sizes'
 import { CgColorPicker } from 'react-icons/cg'
 import { ImFontSize } from 'react-icons/im'
 
-type Props = ThemeEditorProps
+type Props = ThemeEditorButtonProps
 
-export const DefaultThemeEditor: FC<Props> = () => {
+export const DefaultThemeEditor: FC<Props> = (props) => {
   return (
     <ThemeEditor>
-      <ThemeEditorButton />
+      <ThemeEditorButton {...props} />
       <ThemeEditorDrawer>
         <ThemeEditorColors icon={CgColorPicker} title="Colors" />
         <ThemeEditorFontSizes icon={ImFontSize} title="Font Sizes" />

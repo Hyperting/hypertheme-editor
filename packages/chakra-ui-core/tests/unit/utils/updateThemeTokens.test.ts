7cf6a2f45609ac74a112d6606ed9a56bad212cc7
marco <marcon@hyperting.com>
2021-11-15 03:06:00 +0100

    test: fonts

--- packages/chakra-ui-core/tests/unit/utils/updateThemeTokens.test.ts
+++ packages/chakra-ui-core/tests/unit/utils/updateThemeTokens.test.ts
@@ -3,6 +3,13 @@ import {
   setThemeColorsOfPalette,
   setThemePaletteColor,
   setThemeSingleColor,
+  setThemeTypographyFont,
+  setThemeTypographyFonts,
+  setThemeTypographyFontSize,
+  setThemeTypographyLetterSpacing,
+  setThemeTypographyLetterSpacings,
+  setThemeTypographyLineHeight,
+  setThemeTypographyLineHeights,
 } from '../../../src/utils/updateThemeTokens'
 
 describe('updateThemeTokens.ts Unit Group Test', function () {
@@ -172,6 +179,172 @@ describe('updateThemeTokens.ts Unit Group Test', function () {
         expect(cssStyleDeclarationt.getPropertyValue(`--chakra-colors-${colorName}-900`)).toBe(
           colorsInput[colorName][900]
         )
+      })
+    })
+  })
+
+  describe('setThemeTypographyFonts Unit Group Test', function () {
+    describe('setThemeTypographyFont()', function () {
+      it('should set the chakra ui css variable to the correct value', function () {
+        const fontInput = 'strange-variable-value'
+        setThemeTypographyFont('body', fontInput)
+
+        const cssStyleDeclarationt = getComputedStyle(document.documentElement)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-fonts-body`)).toBe(fontInput)
+      })
+    })
+
+    describe('setThemeTypographyFonts()', function () {
+      it('should set the chakra ui css variables to the correct value', function () {
+        const fontInput = {
+          heading: 'strange-variable-value-heading',
+          body: 'strange-variable-value-body',
+          mono: 'strange-variable-value-mono',
+        }
+        setThemeTypographyFonts(fontInput)
+
+        const cssStyleDeclarationt = getComputedStyle(document.documentElement)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-fonts-heading`)).toBe(
+          fontInput.heading
+        )
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-fonts-body`)).toBe(fontInput.body)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-fonts-mono`)).toBe(fontInput.mono)
+      })
+    })
+  })
+
+  describe('setThemeTypograghyFontSizes Unit Group Test', function () {
+    describe('setThemeTypograghyFontSize()', function () {
+      it('should set the chakra ui css variable to the correct value', function () {
+        const fontInput = 'strange-variable-value'
+        setThemeTypographyFontSize('body', fontInput)
+
+        const cssStyleDeclarationt = getComputedStyle(document.documentElement)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-fontSizes-body`)).toBe(fontInput)
+      })
+    })
+
+    describe('setThemeTypograghyFontSizes()', function () {
+      it('should set the chakra ui css variables to the correct value', function () {
+        const fontInput = {
+          heading: 'strange-variable-value-heading',
+          body: 'strange-variable-value-body',
+          mono: 'strange-variable-value-mono',
+        }
+        setThemeTypographyFonts(fontInput)
+
+        const cssStyleDeclarationt = getComputedStyle(document.documentElement)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-fonts-heading`)).toBe(
+          fontInput.heading
+        )
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-fonts-body`)).toBe(fontInput.body)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-fonts-mono`)).toBe(fontInput.mono)
+      })
+    })
+  })
+
+  describe('setThemeTypographyLetterSpacings Unit Group Test', function () {
+    describe('setThemeTypographyLetterSpacing()', function () {
+      it('should set the chakra ui css variable to the correct value', function () {
+        const input = 'strange-variable-value'
+        setThemeTypographyLetterSpacing('normal', input)
+
+        const cssStyleDeclarationt = getComputedStyle(document.documentElement)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-letterSpacings-normal`)).toBe(input)
+      })
+    })
+
+    describe('setThemeTypographyLetterSpacings()', function () {
+      it('should set the chakra ui css variables to the correct value', function () {
+        const input = {
+          tighter: '-0.05',
+          tight: '-0.025',
+          normal: '0',
+          wide: '0.025',
+          wider: '0.05',
+          widest: '0.1',
+        }
+        setThemeTypographyLetterSpacings(input)
+
+        const cssStyleDeclarationt = getComputedStyle(document.documentElement)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-letterSpacings-tighter`)).toBe(
+          input.tighter
+        )
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-letterSpacings-tight`)).toBe(
+          input.tight
+        )
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-letterSpacings-normal`)).toBe(
+          input.normal
+        )
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-letterSpacings-wide`)).toBe(
+          input.wide
+        )
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-letterSpacings-wider`)).toBe(
+          input.wider
+        )
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-letterSpacings-widest`)).toBe(
+          input.widest
+        )
+      })
+    })
+  })
+
+  describe('setThemeTypographyLineHeights Unit Group Test', function () {
+    describe('setThemeTypographyLineHeight()', function () {
+      it('should set the chakra ui css variable to the correct value', function () {
+        const input = 'strange-variable-value'
+        setThemeTypographyLineHeight('base', input)
+
+        const cssStyleDeclarationt = getComputedStyle(document.documentElement)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-base`)).toBe(input)
+      })
+    })
+
+    describe('setThemeTypographyLineHeights()', function () {
+      it('should set the chakra ui css variables to the correct value', function () {
+        const input = {
+          normal: 'normal',
+          none: 1,
+          shorter: 1.25,
+          short: 1.375,
+          base: 1.5,
+          tall: 1.625,
+          taller: '2rem',
+          '3': '0.75rem',
+          '4': '1rem',
+          '5': '1.25rem',
+          '6': '1.5rem',
+          '7': '1.75rem',
+          '8': '2rem',
+          '9': '2.25rem',
+          '10': '2.5rem',
+        }
+        setThemeTypographyLineHeights(input)
+
+        const cssStyleDeclarationt = getComputedStyle(document.documentElement)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-normal`)).toBe(
+          input.normal
+        )
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-none`)).toBe(input.none)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-shorter`)).toBe(
+          input.shorter
+        )
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-short`)).toBe(
+          input.short
+        )
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-base`)).toBe(input.base)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-tall`)).toBe(input.tall)
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-taller`)).toBe(
+          input.taller
+        )
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-3`)).toBe(input['3'])
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-4`)).toBe(input['4'])
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-5`)).toBe(input['5'])
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-6`)).toBe(input['6'])
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-7`)).toBe(input['7'])
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-8`)).toBe(input['8'])
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-9`)).toBe(input['9'])
+        expect(cssStyleDeclarationt.getPropertyValue(`--chakra-lineHeights-10`)).toBe(input['10'])
       })
     })
   })

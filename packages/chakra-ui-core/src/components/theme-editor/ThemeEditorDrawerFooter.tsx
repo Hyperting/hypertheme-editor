501c617ef1176cb643d9952feca3f26d626045ff
sandra <sandra.bote@hyperting.com>
2022-09-30 18:54:25 +0200

    feat: ThemeProperty

--- packages/chakra-ui-core/src/components/theme-editor/ThemeEditorDrawerFooter.tsx
+++ packages/chakra-ui-core/src/components/theme-editor/ThemeEditorDrawerFooter.tsx
@@ -1,42 +1,18 @@
-import {
-  Button,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  Flex,
-  Heading,
-  useColorModeValue,
-  useDisclosure,
-  Text,
-  useColorMode,
-  Box,
-} from '@chakra-ui/react'
+import { Button, Flex, useDisclosure } from '@chakra-ui/react'
 import React, { FC } from 'react'
 import { ThemeDownloadButton } from '.'
-import { ThemeIcon } from '../base'
-import { ThemeEditorDrawerHeader, ThemeEditorDrawerHeaderProps } from './ThemeEditorDrawerHeader'
+import { ThemeExportDrawer } from './ThemeExportDrawer'
 
 export type ThemeEditorDrawerFooterProps = {
   onClose?: () => void
   isMobile?: boolean
   actionButton?: React.ReactNode
-  headerComponent?: React.ReactElement<
-    ThemeEditorDrawerHeaderProps,
-    React.JSXElementConstructor<ThemeEditorDrawerHeaderProps>
-  >
 }
 export const ThemeEditorDrawerFooter: FC<ThemeEditorDrawerFooterProps> = ({
   isMobile,
   actionButton = <ThemeDownloadButton borderRadius="8px" w={{ base: '100%', md: 'initial' }} />,
-  headerComponent = <ThemeEditorDrawerHeader />,
 }) => {
   // const bgColor = useColorModeValue('whiteAlpha.600', 'gray.900')
-  const { colorMode } = useColorMode()
-  const shadow = useColorModeValue('surface', 'surfaceDark')
-  const bgColor = useColorModeValue('white', 'gray.900')
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
@@ -53,36 +29,7 @@ export const ThemeEditorDrawerFooter: FC<ThemeEditorDrawerFooterProps> = ({
       >
         Export theme
       </Button>
-      <Drawer isOpen={isOpen} onClose={onClose} size="md">
-        <DrawerContent bgColor={{ md: bgColor }}>
-          {React.isValidElement(headerComponent) &&
-            React.cloneElement<ThemeEditorDrawerHeaderProps>(
-              headerComponent as React.ReactElement<ThemeEditorDrawerHeaderProps>,
-              {
-                onClose,
-                children: (
-                  <Flex flexDir="column" justifyContent="flex-start" h="80px">
-                    <Text fontSize="1.1rem" fontWeight="normal" color="gray.400" pt={1.5}>
-                      Export
-                    </Text>
-                    <Heading
-                      d="flex"
-                      alignItems="center"
-                      fontSize={{ base: '1.25rem', lg: '1.5rem' }}
-                      h={{ lg: '50%' }}
-                      mb={{ base: 1, lg: 0 }}
-                      pt={0.5}
-                    >
-                      Hyper Theme
-                    </Heading>
-                  </Flex>
-                ),
-              }
-            )}
-          <DrawerBody bgColor="rgba(0,0,0,0)"></DrawerBody>
-          <DrawerFooter></DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+      <ThemeExportDrawer isOpen={isOpen} onClose={onClose} />
     </Flex>
   )
 }
---
+++ packages/chakra-ui-core/src/components/theme-editor/ThemeExportDrawer.tsx
@@ -0,0 +1,181 @@
+import {
+  Checkbox,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerFooter,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  useColorModeValue,
+  Text,
+  useCheckbox,
+  ChakraProvider,
+  chakra,
+  useCheckboxGroup,
+  Box,
+} from '@chakra-ui/react'
+import React, { FC } from 'react'
+import { ThemeEditorDrawerHeaderProps } from '.'
+import { useThemeEditor } from '../../hooks/useThemeEditor'
+import { ThemeEditorDrawerHeader } from './ThemeEditorDrawerHeader'
+
+export type ThemeExportDrawerProps = {
+  isOpen: boolean
+  onClose: () => void
+  headerComponent?: React.ReactElement<
+    ThemeEditorDrawerHeaderProps,
+    React.JSXElementConstructor<ThemeEditorDrawerHeaderProps>
+  >
+}
+
+export const ThemeExportDrawer: FC<ThemeExportDrawerProps> = ({
+  isOpen,
+  onClose,
+  headerComponent = <ThemeEditorDrawerHeader />,
+}) => {
+  const bgColor = useColorModeValue('white', 'gray.900')
+  const { theme } = useThemeEditor()
+  const {
+    colors,
+    fonts,
+    fontSizes,
+    fontWeights,
+    lineHeights,
+    letterSpacings,
+    shadows,
+    radii,
+    space,
+    ...baseProperties
+  } = theme as any
+  const themeLabels = [
+    'Colors',
+    'Font',
+    'Font Sizes',
+    'Font Weight',
+    'Line Height',
+    'Letter Spacing',
+    'Shadows',
+    'Radii',
+    'Space',
+  ]
+  const editableProperties = {
+    colors,
+    fonts,
+    fontSizes,
+    fontWeights,
+    lineHeights,
+    letterSpacings,
+    shadows,
+    radii,
+    space,
+  }
+  const getKeys = (obj) => {
+    return Object.keys(obj)
+  }
+  const { value, getCheckboxProps } = useCheckboxGroup({
+    defaultValue: [],
+  })
+  console.log('theme', theme)
+  console.log('editable', editableProperties)
+  console.log('selected values', value)
+
+  return (
+    <Drawer isOpen={isOpen} onClose={onClose} size="md">
+      <DrawerContent bgColor={{ md: bgColor }}>
+        {React.isValidElement(headerComponent) &&
+          React.cloneElement<ThemeEditorDrawerHeaderProps>(
+            headerComponent as React.ReactElement<ThemeEditorDrawerHeaderProps>,
+            {
+              onClose,
+              children: (
+                <Flex flexDir="column" justifyContent="flex-start" h="80px">
+                  <Text fontSize="1.1rem" fontWeight="normal" color="gray.400" pt={1.5}>
+                    Export
+                  </Text>
+                  <Heading
+                    d="flex"
+                    alignItems="center"
+                    fontSize={{ base: '1.25rem', lg: '1.5rem' }}
+                    h={{ lg: '50%' }}
+                    mb={{ base: 1, lg: 0 }}
+                    pt={0.5}
+                  >
+                    Hyper Theme
+                  </Heading>
+                </Flex>
+              ),
+            }
+          )}
+        <DrawerBody bgColor="rgba(0,0,0,0)" justify="center">
+          <Grid w="100%" templateColumns="repeat(2, 1fr)" templateRows="repeat(5, 1fr)" gap={4}>
+            {getKeys(editableProperties).map((key, index) => {
+              return (
+                <GridItem key={key}>
+                  <ThemeProperty {...getCheckboxProps({ value: key, label: themeLabels[index] })} />
+                </GridItem>
+              )
+            })}
+          </Grid>
+        </DrawerBody>
+        <DrawerFooter></DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+const ThemeProperty = (props) => {
+  const { state, getCheckboxProps, getInputProps, getLabelProps, htmlProps } = useCheckbox(props)
+  const bgColor = useColorModeValue('white', 'gray.700')
+  const checkboxBgColor = useColorModeValue('gray.100', 'gray.800')
+  return (
+    <chakra.label
+      display="flex"
+      flexDir="column"
+      bg="primary.50"
+      outline={state.isChecked ? '3px solid' : 'unset'}
+      outlineColor={state.isChecked ? 'primary.300' : 'unset'}
+      boxShadow={
+        !state.isChecked
+          ? '0px 4px 8px rgba(0, 0, 0, 0.05), 0px 0px 1px rgba(0, 0, 0, 0.2)'
+          : 'unset'
+      }
+      bgColor={state.isChecked ? 'primary.200' : bgColor}
+      rounded="lg"
+      p={4}
+      cursor="pointer"
+      {...htmlProps}
+    >
+      <input {...getInputProps()} hidden />
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        borderRadius="2xl"
+        w="40px"
+        h="40px"
+        colorScheme="primary"
+        bgColor={state.isChecked ? 'primary.500' : checkboxBgColor}
+        {...getCheckboxProps()}
+      >
+        <Flex
+          alignItems="center"
+          justifyContent="center"
+          border="1.5px solid"
+          borderColor={state.isChecked ? 'white' : 'gray.600'}
+          borderRadius="lg"
+          w="20px"
+          h="20px"
+        />
+      </Flex>
+      <Text
+        mt={2}
+        fontWeight="bold"
+        color={state.isChecked ? 'primary.500' : 'gray.600'}
+        {...getLabelProps()}
+      >
+        {props.label}
+      </Text>
+    </chakra.label>
+  )
+}

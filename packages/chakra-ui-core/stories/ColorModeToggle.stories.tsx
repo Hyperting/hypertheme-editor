d78ef1d6c4e7574f43f18098161565ae80f45d65
sandra <sandra.bote@hyperting.com>
2024-07-25 09:54:47 +0200

    fix: ColorModeToggle onload error & broken ui

--- packages/chakra-ui-core/stories/ColorModeToggle.stories.tsx
+++ packages/chakra-ui-core/stories/ColorModeToggle.stories.tsx
@@ -1,34 +1,39 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react'
-import { Meta } from '@storybook/react'
-import { Stack } from '@chakra-ui/react'
-import { ThemeEditorProvider } from '../src'
-import ColorModeToggle from '../src/components/base/ColorModeToggle'
+import React from "react";
+import { Meta } from "@storybook/react";
+import { ChakraProvider, Stack } from "@chakra-ui/react";
+import { ThemeEditorProvider } from "../src";
+import ColorModeToggle from "../src/components/base/ColorModeToggle";
+import { theme } from "@hypertheme-editor/chakra-ui-theme";
 
 export default {
-  title: 'HyperThemeEditor/chakra-ui-core/ColorModeToggle',
+  title: "HyperThemeEditor/chakra-ui-core/ColorModeToggle",
   component: ColorModeToggle,
   argTypes: {},
-} as Meta<typeof ColorModeToggle>
+} as Meta<typeof ColorModeToggle>;
 
 export const Sizes = (args) => {
   return (
     <ThemeEditorProvider>
-      <Stack>
-        <ColorModeToggle size="sm" colorScheme="primary" />
-        <ColorModeToggle size="md" />
-        <ColorModeToggle size="lg" />
-      </Stack>
+      <ChakraProvider theme={theme}>
+        <Stack>
+          <ColorModeToggle size="sm" colorScheme="primary" />
+          <ColorModeToggle size="md" />
+          <ColorModeToggle size="lg" />
+        </Stack>
+      </ChakraProvider>
     </ThemeEditorProvider>
-  )
-}
+  );
+};
 
 export const ShowLabel = (args) => {
   return (
     <ThemeEditorProvider>
-      <Stack>
-        <ColorModeToggle size="lg" showLabel />
-      </Stack>
+      <ChakraProvider theme={theme}>
+        <Stack>
+          <ColorModeToggle size="lg" showLabel />
+        </Stack>
+      </ChakraProvider>
     </ThemeEditorProvider>
-  )
-}
+  );
+};

419d8c107a0cc7f455318aae01afc096ab419432
sandra <sandra.bote@hyperting.com>
2024-08-23 15:14:50 +0200

    feat: migrating chakra-ui-colors (wip)

---
+++ packages/chakra-ui-colors/src/index.ts
@@ -0,0 +1,16 @@
+import { ThemeEditorColors } from './ThemeEditorColors'
+import ThemeEditorColorItem from './ThemeEditorColorItem'
+import ThemeEditorPalette from './ThemeEditorPalette'
+
+import ThemeEditorPaletteDrawer from './ThemeEditorPaletteDrawer'
+import { ThemeEditorPalettePopoverForm } from './ThemeEditorPalettePopoverForm'
+import { generatePalette } from './generateColorPalette'
+
+export {
+  ThemeEditorColors,
+  ThemeEditorColorItem,
+  ThemeEditorPalettePopoverForm,
+  ThemeEditorPaletteDrawer,
+  ThemeEditorPalette,
+  generatePalette,
+}

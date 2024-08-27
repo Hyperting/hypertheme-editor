3af96f48e006306b10a68c4a6f9111798fc1bf21
sandra <sandra.bote@hyperting.com>
2024-08-27 12:23:07 +0200

    fix: hypertheme-chakra-ui button style

--- packages/hypertheme-chakra-ui/stories/ThemeEditor.stories.tsx
+++ packages/hypertheme-chakra-ui/stories/ThemeEditor.stories.tsx
@@ -1,19 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react'
-import { ComponentMeta } from '@storybook/react'
-import { Button } from '@chakra-ui/react'
-import { CgColorPicker, CgEditShadows, CgSpaceBetween } from 'react-icons/cg'
-import { ImFontSize } from 'react-icons/im'
-import { BiText } from 'react-icons/bi'
-import { MdRoundedCorner } from 'react-icons/md'
+import React from "react";
+import { Meta } from "@storybook/react";
+import { Button } from "@chakra-ui/react";
+import { CgColorPicker, CgEditShadows, CgSpaceBetween } from "react-icons/cg";
+import { ImFontSize } from "react-icons/im";
+import { BiText } from "react-icons/bi";
+import { MdRoundedCorner } from "react-icons/md";
 import //   ThemeEditorButton,
 // ThemeEditorProvider,
 //   ThemeEditorRootPanel,
 //   ThemeEditorDrawer,
 //   ThemeEditor,
-'@hypertheme-editor/chakra-ui-core'
-import { ThemeEditorColors } from '@hypertheme-editor/chakra-ui-colors'
-import { ThemeEditorFontSizes } from '@hypertheme-editor/chakra-ui-font-sizes'
+"@hypertheme-editor/chakra-ui-core";
+import { ThemeEditorColors } from "@hypertheme-editor/chakra-ui-colors";
+import { ThemeEditorFontSizes } from "@hypertheme-editor/chakra-ui-font-sizes";
 import {
   HyperThemeEditor,
   ThemeEditorButton,
@@ -21,32 +21,36 @@ import {
   ThemeEditorRootPanel,
   ThemeEditorDrawer,
   ThemeEditor,
-} from '../src'
+} from "../src";
 
 export default {
-  title: 'HyperThemeEditor/hypertheme-chakra-ui/ThemeEditor Community',
+  title: "HyperThemeEditor/hypertheme-chakra-ui/ThemeEditor Community",
   component: ThemeEditor,
   argTypes: {},
-} as ComponentMeta<typeof ThemeEditor>
+} as Meta<typeof ThemeEditor>;
 
 export const DefaultEditor = (args) => (
   <ThemeEditorProvider>
-    <Button colorScheme="blue">Button</Button>
-    <HyperThemeEditor {...args} />
+    <ThemeEditor {...args}>
+      <Button colorScheme="blue">Button</Button>
+      <HyperThemeEditor {...args} />
+    </ThemeEditor>
   </ThemeEditorProvider>
-)
+);
 
 export const DefaultEditorWithHiddenCredits = (args) => (
   <ThemeEditorProvider>
-    <Button colorScheme="blue">Button</Button>
-    <HyperThemeEditor {...args} />
+    <ThemeEditor {...args}>
+      <Button colorScheme="blue">Button</Button>
+      <HyperThemeEditor {...args} />
+    </ThemeEditor>
   </ThemeEditorProvider>
-)
+);
 
 export const WithRootPanel = (args) => (
   <ThemeEditorProvider>
-    <Button colorScheme="blue">Button</Button>
     <ThemeEditor {...args}>
+      <Button colorScheme="blue">Button</Button>
       <ThemeEditorButton />
       <ThemeEditorDrawer>
         <ThemeEditorRootPanel icon={CgColorPicker} title="Colors">
@@ -58,12 +62,12 @@ export const WithRootPanel = (args) => (
       </ThemeEditorDrawer>
     </ThemeEditor>
   </ThemeEditorProvider>
-)
+);
 
 export const WithoutRootPanel = (args) => (
   <ThemeEditorProvider>
-    <Button colorScheme="blue">Button</Button>
     <ThemeEditor {...args}>
+      <Button colorScheme="blue">Button</Button>
       <ThemeEditorButton />
       <ThemeEditorDrawer>
         <ThemeEditorColors icon={CgColorPicker} title="Colors" />
@@ -71,4 +75,4 @@ export const WithoutRootPanel = (args) => (
       </ThemeEditorDrawer>
     </ThemeEditor>
   </ThemeEditorProvider>
-)
+);
